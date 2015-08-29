65742a5dea349e42532ec4c1839bd3a2cb36bf4a
Burton Samograd <burton.samograd@gmail.com>
2015-08-29 11:51:45 -0600

    Initial commit.

---
+++ init.el
@@ -0,0 +1,9 @@
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+
+(global-set-key "\M-;" 'other-window)
+
+(custom-set-faces
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 83 :width normal)))))
+
