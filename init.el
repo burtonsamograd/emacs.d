8f12dcc1346eeac1c3c0e1eeb69909c86688cd55
Burton Samograd <burton.samograd@gmail.com>
2015-08-29 11:56:40 -0600

    Update ansi-color-drop-regexp to rid the shell buffer of node garbage characters.

--- init.el
+++ init.el
@@ -7,6 +7,10 @@
 (add-to-list 'load-path "~/.emacs.d")
 (require 'grep-buffers)
 
+;; Get rid of the node garbage characters
+(setq ansi-color-drop-regexp
+  "\033\\[\\([ABCDsuK]\\|[12][JK]\\|=[0-9]+[hI]\\|[0-9;]*[Hf]\\|\\?[0-9]+[hl]\\|[0-9][GJ]\\)")
+
 (custom-set-faces
  '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 83 :width normal)))))
 
