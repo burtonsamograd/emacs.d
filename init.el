88b950b8360ecb16ad91f0f6718bcb16410053e2
Burton Samograd <burton.samograd@gmail.com>
2015-08-29 11:52:23 -0600

    Add grep-buffers.el.

--- init.el
+++ init.el
@@ -4,6 +4,9 @@
 
 (global-set-key "\M-;" 'other-window)
 
+(add-to-list 'load-path "~/.emacs.d")
+(require 'grep-buffers)
+
 (custom-set-faces
  '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 83 :width normal)))))
 
