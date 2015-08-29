5c63ba1b34c2d508552e23877b256a64aeff94d9
Burton Samograd <burton.samograd@gmail.com>
2015-08-29 12:53:24 -0600

    Conditional font size based on hostname; Indentation change; grep-buffers.el move.

--- init.el
+++ init.el
@@ -13,8 +13,8 @@
 
 ;; Get rid of the node garbage characters
 (setq ansi-color-drop-regexp
-  "\033\\[\\([ABCDsuK]\\|[12][JK]\\|=[0-9]+[hI]\\|[0-9;]*[Hf]\\|\\?[0-9]+[hl]\\|[0-9][GJ]\\)")
+      "\033\\[\\([ABCDsuK]\\|[12][JK]\\|=[0-9]+[hI]\\|[0-9;]*[Hf]\\|\\?[0-9]+[hl]\\|[0-9][GJ]\\)")
 
 (custom-set-faces
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 83 :width normal)))))
+ `(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height ,(if (string= system-name "burton-ThinkPad-X300-2") 120 83) :width normal)))))
 
