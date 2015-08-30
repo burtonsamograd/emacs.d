3bd7fc09380b74b50c396327da73e996faa95219
Burton Samograd <burton.samograd@gmail.com>
2015-08-29 22:47:29 -0600

    Make node garbage character killing more accurate.

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
 ;; Get rid of the node garbage characters
 (require 'shell)
 (setq ansi-color-drop-regexp
-      "\033\\[\\([ABCDsuK]\\|[12][JK]\\|=[0-9]+[hI]\\|[0-9;]*[Hf]\\|\\?[0-9]+[hl]\\|[0-9][GJ]\\)")
+      "\033\\[\\([ABCDsuK]\\|[12][JK]\\|=[0-9]+[hI]\\|[0-9;]*[Hf]\\|\\?[0-9]+[hl]\\|[13]G\\|0J\\)")
 
 (custom-set-faces
  `(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height ,(if (string= system-name "burton-ThinkPad-X300-2") 120 83) :width normal)))))
